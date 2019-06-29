1455b1d13cb2ea9e5035c5a0dd65e54452435edd
HeeCheolKim <msn9975@gmail.com>
2019-06-29 16:27:25 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ChatFrameTemplate from './components/templates/chat-frame-template';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <ChatFrameTemplate />
     </div>
   );
 };
---
+++ src/components/templates/chat-frame-template/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      header
+      <div>
+        <input type="text"/>
+        <button>전송</button>
+      </div>
+    </div>
+  )
+};
+
+export default Index;
