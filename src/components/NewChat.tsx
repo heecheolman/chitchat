59ac6cfa260d25a7f5686a6fdc417c147bcd6f33
HeeCheolKim <msn9975@gmail.com>
2019-06-30 21:34:31 +0900

    feat: 채팅방 생성

--- src/components/NewChat.tsx
+++ src/components/NewChat.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from 'react-apollo-hooks';
+import { Store } from '../store';
 
-const NewChat: React.FC = () => (
-  <>
-    <button onClick={() => {
-      const chatRoomTitle = prompt('채팅방 입력');
+const CREATE_CHAT_ROOM = gql`
+  mutation createChatRoom ($userId: Int!, $title: String!) {
+    createChatRoom(userId: $userId, title: $title) {
+      id
+      title
+      users {
+        id
+        userName
+      }
+      messages {
+        id
+        content
+        createdBy {
+          id
+          userName
+        }
+        createdAt
+      }
+    }
+  }
+`;
 
-    }}>새로운채팅 만들기</button>
-  </>
-);
+const NewChat: React.FC = () => {
+  const userId = +Store.instance.id;
+  const mutation = useMutation(CREATE_CHAT_ROOM, {
+    variables: {
+      userId,
+      title: '1번채팅방'
+    },
+    update: (proxy, { data }) => {
+      console.log('create chatRoom data', data);
+    }
+  });
+
+  return (
+    <>
+      <button onClick={() => {
+        mutation();
+      }}>새로운채팅 만들기</button>
+    </>
+  );
+}
 
 export default NewChat;
--- src/containers/ChatListTemplate.tsx
+++ src/containers/ChatListTemplate.tsx
@@ -1,14 +1,83 @@
 import React from 'react';
-import { Store } from '../store';
+import gql from 'graphql-tag';
 
+import { Store } from '../store';
 import ChatHeader from '../components/ChatHeader';
 import NewChat from '../components/NewChat';
+import { Query } from 'react-apollo';
 
-const ChatListTemplate: React.FC = () => (
-  <>
-    <ChatHeader userName={Store.instance.userName} />
-    <NewChat />
-  </>
-);
+const GET_CHAT_ROOMS = gql`
+  query {
+    chatRooms {
+      id
+      title
+    }
+  }
+`;
+
+const CHAT_ROOM_CREATED = gql`
+  subscription {
+    chatRoomCreated {
+      id
+      title
+      users {
+        id
+        userName
+      }
+    }
+  }
+`;
+
+let unsubscribe: any = null;
+
+const ChatListTemplate: React.FC = () => {
+  return (
+    <>
+      <Query query={GET_CHAT_ROOMS}>
+        {({ loading, data, subscribeToMore }: any) => {
+
+          if (loading) {
+            return null;
+          }
+
+          if (!unsubscribe) {
+            unsubscribe = subscribeToMore({
+              document: CHAT_ROOM_CREATED,
+              updateQuery: (prev: any, { subscriptionData }: any) => {
+                if (!subscriptionData.data) {
+                  return prev;
+                }
+                const { chatRoomCreated } = subscriptionData.data;
+                console.log('prev', prev);
+                console.log('chatRoomCreated', chatRoomCreated);
+                return {
+                  ...prev,
+                  chatRooms: [
+                    ...prev.chatRooms,
+                    chatRoomCreated
+                  ]
+                }
+              }
+            });
+          }
+
+          return (
+            <>
+              <ChatHeader userName={Store.instance.userName} />
+              {
+                data.chatRooms.map((chatRoom: any, index: number) => {
+                  return (
+                    <li key={index}>{chatRoom.id}: {chatRoom.title}</li>
+                  )
+                })
+              }
+              <NewChat />
+            </>
+          )
+        }}
+      </Query>
+    </>
+  );
+};
 
 export default ChatListTemplate;
