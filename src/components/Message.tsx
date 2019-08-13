4b2edf30b655369727458d6e3a128de838188e9b
HeeCheolKim <msn9975@gmail.com>
2019-08-13 23:51:02 +0900

    feat: 메세지 정렬 및 스타일 추가

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IMessage } from '../interfaces';
 import styles from './Message.module.scss';
 import moment from 'moment';
+import { Store } from '../store';
 /*
 {
         "id": 0,
@@ -14,15 +15,19 @@ import moment from 'moment';
       },
  */
 
-const Message: React.FC<{ message: IMessage }> = ({ message }) =>
-  <>
-    <div className={styles.messageBox}>
-      <span className={styles.userName}>{message.createdBy.userName}</span>
-      <div className={styles.messageContentBox}>
-        <span className={styles.message}>{message.content}</span>
+const Message: React.FC<{ message: IMessage }> = ({ message }) => {
+  const isMe = +Store.instance.id === message.createdBy.id;
+  return (
+    <>
+      <div className={`${styles.messageBox} ${isMe ? styles.messageAlignRight : styles.messageAlignLeft}`}>
+        <span className={styles.userName}>{message.createdBy.userName}</span>
+        <div className={styles.messageContentBox}>
+          <span className={styles.message}>{message.content}</span>
+        </div>
+        <span className={styles.timestamp}>{moment(parseInt(message.createdAt)).format('a hh:mm:ss')}</span>
       </div>
-      <span className={styles.timestamp}>{moment(parseInt(message.createdAt)).format('a hh:mm:ss')}</span>
-    </div>
-  </>;
+    </>
+  )
+};
 
 export default Message;
