5bf3abd0307c0fd314a9d15b302297b566153da5
HeeCheolKim <msn9975@gmail.com>
2019-08-15 21:44:17 +0900

    fix: 새로운 채팅 class 컴포넌트로 변경 및 채팅룸 리스트 통신 버그 픽스

--- src/components/ChatRoomList/ChatRoomList.tsx
+++ src/components/ChatRoomList/ChatRoomList.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import { FiX } from 'react-icons/fi';
-
-import NewChat from '../NewChat';
 import styles from './ChatRoomList.module.scss';
 import ChatRoom from './ChatRoom';
-import { useMutation } from 'react-apollo-hooks';
 import { Store } from '../../store';
 import ChatHeader from '../ChatHeader';
+import { NewChatPrompt } from '../NewChatPrompt';
 
 const CHAT_ROOMS_QUERY = gql`
   query {
@@ -34,129 +31,78 @@ const CHAT_ROOM_SUBSCRIPTION = gql`
   }
 `;
 
-const CREATE_CHAT_ROOM = gql`
-    mutation createChatRoom ($userId: Int!, $title: String!, $description: String!) {
-        createChatRoom(userId: $userId, title: $title, description: $description) {
-            id
-            title
-            description
-            users {
-                id
-                userName
-            }
-            messages {
-                id
-                content
-                createdBy {
-                    id
-                    userName
-                }
-                createdAt
-            }
-        }
-    }
-`;
 
 let subscription: any = null;
 
-const ChatRoomList: React.FC = () => {
-  const [backdrop, setBackdrop] = useState(false);
-  const [roomTitle, setRoomTitle] = useState('');
-  const [roomDesc, setRoomDesc] = useState('');
-  const userId = +Store.instance.id;
+class ChatRoomList extends React.Component<any, { userId: number; backdrop: boolean; }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      userId: +Store.instance.id,
+      backdrop: false,
+    };
+  }
 
-  const newChatMutation = useMutation(CREATE_CHAT_ROOM, {
-    variables: {
-      userId,
-      title: roomTitle,
-      description: roomDesc,
-    },
-  });
+  setBackdrop(hasBackdrop: boolean): void {
+    this.setState({
+      backdrop: hasBackdrop
+    })
+  }
 
-  useEffect(() => {
-    return () => {
-      subscription = null;
-    }
-  });
+  componentWillUnmount(): void {
+    subscription = null;
+  }
 
-  const createButtonDisabled = !roomTitle.length || !roomDesc.length;
-
-  const newChat = () => {
-    newChatMutation();
-    setRoomTitle('');
-    setRoomDesc('');
-    setBackdrop(false);
-  };
-
-  return (
-    <>
-      <Query query={CHAT_ROOMS_QUERY}
-             fetchPolicy={'network-only'}
-      >
-        {({ loading, data, subscribeToMore }: any) => {
-          if (loading) {
-            return null;
-          }
-          if (!subscription) {
-            subscription = subscribeToMore({
-              document: CHAT_ROOM_SUBSCRIPTION,
-              updateQuery: (prev: any, { subscriptionData }: any) => {
-                if (!subscriptionData.data) {
-                  return prev;
+  render() {
+    return (
+      <>
+        <Query query={CHAT_ROOMS_QUERY}
+               fetchPolicy={'network-only'}
+        >
+          {({ loading, data, subscribeToMore }: any) => {
+            if (loading) {
+              return null;
+            }
+            if (!subscription) {
+              subscription = subscribeToMore({
+                document: CHAT_ROOM_SUBSCRIPTION,
+                updateQuery: (prev: any, { subscriptionData }: any) => {
+                  if (!subscriptionData.data) {
+                    return prev;
+                  }
+                  const { chatRoomCreated } = subscriptionData.data;
+                  return {
+                    ...prev,
+                    chatRooms: [
+                      ...prev.chatRooms,
+                      chatRoomCreated
+                    ]
+                  }
                 }
-                const { chatRoomCreated } = subscriptionData.data;
-                return {
-                  ...prev,
-                  chatRooms: [
-                    ...prev.chatRooms,
-                    chatRoomCreated
-                  ]
-                }
-              }
-            });
-          }
-          return (
-            <>
-              <ChatHeader prevMeta={{ canPrev: true, url: '/' }} title={'채팅목록'} />
-              <div className={backdrop ? styles.backdrop : styles.displayNone}>
-                <div className={styles.backdropNewChatWrap}>
-                  <div className={styles.newChatHeader}>
-                    <button className={styles.close} onClick={() => setBackdrop(false)}><FiX /></button>
-                  </div>
-                  <div className={styles.newChatBody}>
-                    <div className="simple-input-wrap">
-                      <span className="simple-input-label">제목</span>
-                      <input className="simple-input" type="text" maxLength={20} placeholder="20자 이내로 작성해주세요."
-                             onChange={(e) => setRoomTitle(e.target.value)} />
-                    </div>
-                    <div className="simple-input-wrap">
-                      <span className="simple-input-label">설명</span>
-                      <input className="simple-input" type="text" maxLength={30} placeholder="30자 이내로 작성해주세요."
-                             onChange={(e) => setRoomDesc(e.target.value)}/>
-                    </div>
-                    <div className={styles.newChatActionButtonWrap}>
-                      <button disabled={createButtonDisabled}
-                              className={`simple-button ${styles.newChatActionButton} ${createButtonDisabled ? 'simple-button--disabled' : ''}`}
-                              onClick={() => newChat()}
-                      >생성</button>
-                    </div>
-                  </div>
+              });
+            }
+            return (
+              <>
+                <ChatHeader prevMeta={{ canPrev: true, url: '/' }} title={'채팅목록'} />
+                <NewChatPrompt backdrop={this.state.backdrop} setBackdrop={this.setBackdrop.bind(this)} />
+                <div className={styles.chatRooms}>
+                  {
+                    data.chatRooms.map((chatRoom: any) => (
+                      <ChatRoom key={chatRoom.id} chatRoom={chatRoom} />
+                    ))
+                  }
                 </div>
-              </div>
-              <div className={styles.chatRooms}>
-                {
-                  data.chatRooms.map((chatRoom: any) => (
-                    <ChatRoom key={chatRoom.id} chatRoom={chatRoom} />
-                  ))
-                }
-              </div>
-              <NewChat backdrop={setBackdrop} />
-            </>
-          )
-        }}
-      </Query>
-    </>
-  );
+                <div className={styles.newChatWrap}>
+                  <button className={`simple-button ${styles.newChatButton}`}
+                          onClick={() => this.setBackdrop(true)}>새로운채팅</button>
+                </div>
+              </>
+            )
+          }}
+        </Query>
+      </>
+    );
+  };
 };
 
 export default ChatRoomList;
---
+++ src/components/NewChatPrompt.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import styles from './NewChatPrompt.module.scss';
+import { FiX } from 'react-icons/fi';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Store } from '../store';
+
+const CREATE_CHAT_ROOM = gql`
+    mutation createChatRoom ($userId: Int!, $title: String!, $description: String!) {
+        createChatRoom(userId: $userId, title: $title, description: $description) {
+            id
+            title
+            description
+            users {
+                id
+                userName
+            }
+            messages {
+                id
+                content
+                createdBy {
+                    id
+                    userName
+                }
+                createdAt
+            }
+        }
+    }
+`;
+
+export class NewChatPrompt extends React.Component<{ backdrop: boolean; setBackdrop: Function; }, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      userId: +Store.instance.id,
+      title: '',
+      description: '',
+    };
+  }
+
+  setTitle(value: string): void {
+    this.setState({
+      title: value,
+    });
+  }
+
+  setDescription(value: string): void {
+    this.setState({
+      description: value,
+    });
+  }
+
+  createChatRoom(newChatMutation: any): void {
+    newChatMutation();
+    this.setTitle('');
+    this.setDescription('');
+    this.props.setBackdrop(false);
+  }
+
+  get buttonDisabled() {
+    return !this.state.title.length || !this.state.description.length;
+  }
+
+  render() {
+    return (
+      <div className={this.props.backdrop ? styles.backdrop : styles.displayNone}>
+        <div className={styles.backdropNewChatWrap}>
+          <div className={styles.newChatHeader}>
+            <button className={styles.close} onClick={() => this.props.setBackdrop(false)}><FiX /></button>
+          </div>
+          <div className={styles.newChatBody}>
+            <div className="simple-input-wrap">
+              <span className="simple-input-label">제목</span>
+              <input className="simple-input" type="text" maxLength={20} placeholder="20자 이내로 작성해주세요."
+                     value={this.state.title}
+                     onChange={(e) => this.setTitle(e.target.value)} />
+            </div>
+            <div className="simple-input-wrap">
+              <span className="simple-input-label">설명</span>
+              <input className="simple-input" type="text" maxLength={30} placeholder="30자 이내로 작성해주세요."
+                     value={this.state.description}
+                     onChange={(e) => this.setDescription(e.target.value)}/>
+            </div>
+            <div className={styles.newChatActionButtonWrap}>
+              <Mutation mutation={CREATE_CHAT_ROOM} variables={{
+                userId: this.state.userId,
+                title: this.state.title,
+                description: this.state.description,
+              }}>
+                {(newChatMutation: any) => (
+                  <button disabled={this.buttonDisabled}
+                          className={`simple-button ${styles.newChatActionButton} ${this.buttonDisabled ? 'simple-button--disabled' : ''}`}
+                          onClick={() => this.createChatRoom(newChatMutation)}
+                  >생성</button>
+                )}
+              </Mutation>
+            </div>
+          </div>
+          </div>
+      </div>
+    );
+  }
+}
